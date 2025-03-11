ea2aa19fa58808820789244b8fb363cd213fed41
harigovind11 <harigovindsreejith7@gmail.com>
2025-03-11 10:05:35 +0530

    NetworkVariable with Custom Data Type, INetworkSerializable

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -1,13 +1,48 @@
 using System;
+using Unity.Collections;
 using Unity.Netcode;
 using UnityEngine;
-using UnityEngine.InputSystem;  
+using UnityEngine.InputSystem;
+using Random = UnityEngine.Random;
+
+
 public class PlayerNetwork : NetworkBehaviour
 {
+    private NetworkVariable<MyCustomData> randomNumber = new NetworkVariable<MyCustomData>(new MyCustomData
+    {
+        _int = 56,
+        _bool = true
+    },NetworkVariableReadPermission.Everyone,NetworkVariableWritePermission.Owner);
+
+    public struct MyCustomData : INetworkSerializable
+    {
+        public int _int;
+        public bool _bool;
+        public FixedString128Bytes message;
+
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        {
+            serializer.SerializeValue(ref _int);
+            serializer.SerializeValue(ref _bool);
+            serializer.SerializeValue(ref message);
+        }
+    }
+    public override void OnNetworkSpawn()
+    {
+        randomNumber.OnValueChanged += ((MyCustomData previousValue, MyCustomData newValue) =>
+        {
+            Debug.Log(OwnerClientId + "; " + newValue._int + "; " + newValue._bool + "; " + newValue.message);
+        });
+    }
     private void Update()
     {
+       
         if(!IsOwner) return;
         
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            randomNumber.Value =new MyCustomData{_int = 64,_bool = false,message = "Hello"};
+        }
         Vector3 moveDirection = new Vector3(0, 0, 0);
 
         if (Input.GetKey(KeyCode.W)) moveDirection.z = +1f;
